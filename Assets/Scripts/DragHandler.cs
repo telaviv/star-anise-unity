923e45a660c1f22a3ea1b845fee68c11d2852d30
Shawn Krisman <krisman.shawn@gmail.com>
2016-01-26 12:00:51 -0800

    card is draggable.

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject itemBeingDragged;
+	Vector3 startPosition; 
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = gameObject;
+		startPosition = transform.position;
+	}
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		transform.position = startPosition;
+	}
+	#endregion
+}
