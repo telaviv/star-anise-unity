09f9101eb13f4579f309b64398b7d3176b8fccb6
Shawn Krisman <krisman.shawn@gmail.com>
2016-02-04 17:48:23 -0800

    trying to get drag handlers set up.

--- Assets/Scripts/DragHandler.cs
+++ Assets/Scripts/DragHandler.cs
@@ -4,12 +4,24 @@ using UnityEngine.EventSystems;
 
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 	public static GameObject itemBeingDragged;
-	Vector3 startPosition; 
+	private Canvas canvas;
+	private Transform cardHand;
+	Vector3 startPosition;
+
+	public void Start() {
+		canvas = GetComponentInParent<Canvas> ();
+		cardHand = transform.parent;
+	}
+		
+	void OnMouseEnter() {
+		Debug.Log (transform.position);
+	}
 
 	#region IBeginDragHandler implementation
 	public void OnBeginDrag (PointerEventData eventData)
 	{
 		itemBeingDragged = gameObject;
+		transform.SetParent (canvas.transform, true);
 		startPosition = transform.position;
 	}
 	#endregion
@@ -25,6 +37,7 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 	#region IEndDragHandler implementation
 	public void OnEndDrag (PointerEventData eventData)
 	{
+		transform.SetParent(cardHand);
 		transform.position = startPosition;
 	}
 	#endregion
